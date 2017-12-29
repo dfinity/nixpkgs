ddcf8864cac5aef92a39ee6b6573f96a254f3c40
Frederik Rietdijk <fridh@fridh.nl>
2017-12-29 20:26:38 +0100

    python.pkgs.attrs: 17.2.0 -> 17.3.0

--- pkgs/development/python-modules/attrs/default.nix
+++ pkgs/development/python-modules/attrs/default.nix
@@ -4,21 +4,24 @@
 buildPythonPackage rec {
   name = "${pname}-${version}";
   pname = "attrs";
-  version = "17.2.0";
+  version = "17.3.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "04gx08ikpk26wnq22f7l42gapcvk8iz1512r927k6sadz6cinkax";
+    sha256 = "c78f53e32d7cf36d8597c8a2c7e3c0ad210f97b9509e152e4c37fa80869f823c";
   };
 
   # macOS needs clang for testing
-  buildInputs = [
+  checkInputs = [
     pytest hypothesis zope_interface pympler coverage six
   ] ++ lib.optionals (stdenv.isDarwin) [ clang ];
 
   checkPhase = ''
     py.test
   '';
+
+  # To prevent infinite recursion with pytest
+  doCheck = false;
 
   meta = with lib; {
     description = "Python attributes without boilerplate";
